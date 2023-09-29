2a11237de53096c20ff67f0ddd6f0f20e4f211be
HighDrizzDev <usvirux@gmail.com>
2023-09-29 02:08:58 -0400

    Agregar archivos de proyecto.

---
+++ MyMVVMProject/Model/HelloWorldModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMVVMProject.Model
+{
+    public class HelloWorldModel
+    {
+
+        private List<string> _repositoryData;
+
+        public string ImportantInfo
+        {
+            get
+            {
+                return ConcatenateData(_repositoryData);
+            }
+            set
+            {
+
+            }
+        }
+
+        private string ConcatenateData(List<string> dataList)
+        {
+            string cadena = dataList.ElementAt(0) + ", " + dataList.ElementAt(1) + "!";
+
+            return cadena;
+        }
+
+
+        private List<string> GetInfo()
+        {
+
+            _repositoryData = new List<string>() 
+            {
+                "Hello",
+                "World \t Leandro Batista"
+            };
+
+            return _repositoryData;
+
+        }
+
+
+        public HelloWorldModel()
+        {
+           _repositoryData = GetInfo();
+        }
+
+    }
+}
---
+++ MyMVVMProject/ViewModel/HelloWorldViewModel.cs
@@ -0,0 +1,57 @@
+﻿using MyMVVMProject.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMVVMProject.ViewModel
+{
+    public class HelloWorldViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _helloString;
+
+        public string HelloString
+        {
+            get
+            {
+                return _helloString;
+            }
+            set
+            {
+                if( _helloString != value )
+                {
+                    _helloString = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+
+        protected void OnPropertyChanged([CallerMemberName] string nombrePropiedad = "")
+        {
+
+            if(nombrePropiedad != null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombrePropiedad));
+            }
+
+        }
+
+
+        public HelloWorldViewModel()
+        {
+            HelloWorldModel helloWorldModel = new HelloWorldModel();
+
+            _helloString = helloWorldModel.ImportantInfo;
+
+        }
+
+
+    }
+}
